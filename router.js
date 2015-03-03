97d3d8fb8768da3a3430bab08292f1f51c544674
Tyler Brown and Whitney Reans <wreans@yahoo.com>
2015-03-03 12:08:29 -0800

    Add routes and pages for the headlines on the main page

--- router.js
+++ router.js
@@ -1,3 +1,7 @@
 Bustle.Router.map(function() {
-  this.resource('headlines');
+  this.resource('headlines', {path: '/'});
+  this.resource('manBun');
+  this.resource('hip');
+  this.resource('facialHair');
+  this.resource('granola');
 });
