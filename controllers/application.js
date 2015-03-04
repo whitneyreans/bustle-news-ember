c8aad4fc4725a596d00a8f6666403cfffa2e6059
Tyler Brown and Whitney Reans <elementalplug@gmail.com>
2015-03-03 16:52:23 -0800

    Add dropdown social links menu

--- controllers/application.js
+++ controllers/application.js
@@ -1,13 +1,11 @@
 Bustle.ApplicationController = Ember.Controller.extend({
   showMenu: false,
   actions: {
-    toggleMenu: function() {
+    toggleMenuOn: function() {
       this.set('showMenu', true);
-      // if (showMenu = true) {
-      //   this.set('showMenu', false);
-      // } else {
-      //   this.set('showMenu', true);
-      // }
+    },    
+    toggleMenuOff: function() {
+      this.set('showMenu', false);
     }
   }
 });
